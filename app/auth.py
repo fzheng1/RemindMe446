40bca9d19ecfd34395ceaf5b908e9709d51dd145
fzheng1 <kaifenzheng@gmail.com>
2022-07-23 17:35:35 -0400

    bugfix

--- app/auth.py
+++ app/auth.py
@@ -2,7 +2,7 @@ from flask import Blueprint, jsonify, request
 from flask_login import login_user, login_required, logout_user
 from sqlalchemy import desc
 from werkzeug.security import generate_password_hash, check_password_hash
-from .models import User
+from .models import User, Group
 from . import db
 from typing import List, Dict
 
